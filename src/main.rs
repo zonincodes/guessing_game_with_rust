0a49f8def522c4b19efdcda1e0f0040f2efb64b2
zonin <business.kariukimartin@gmail.com>
2024-03-06 06:52:25 +0300

    dereferencing raw pointers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use learn::Post;
 
 fn main(){
+    let mut num = 5;
+
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
+
+}
+fn _oop_impl() {
     println!("Hello World!");
     let mut post = Post::new();
     post.add_text("I ate a salad for lunch today");
